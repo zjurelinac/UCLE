2fdc24f6328a9a1c2f802aec0767ede5bab9b974
Zvonimir Jurelinac <zjurelinac@gmail.com>
2018-03-10 19:51:33 +0100

    Frisc HLS - Memory and ALU operations implemented and tested

--- ucle/core/src/hls/frisc.cpp
+++ ucle/core/src/hls/frisc.cpp
@@ -4,13 +4,16 @@
 //  Desired features in productin version:
 //      - register<size> type
 //      - signed_cast
-//      - fixed-size types (int16, int32...)
+//      - fixed-size types (int16, int32...) -> exist in C++11
 //      - uniform HLS interface
 //      - header file
 //      - OO inheritance
 //      - binary util library
 //      - info and status queries
 //      - policy-based execution?
+//      - SR-flags class - mapping between flags and status register bits
+//      - memory class - test overflows and sigsegvs
+//      - exceptions/expecteds
 //******************************************************************************
 
 #include <cstdio>
@@ -35,10 +38,23 @@ word sign_extend20(word val);
 
 enum class hls_state { initialized, loaded, running, stopped, terminated, exception };
 
+std::ostream& operator<<(std::ostream& os, hls_state hs) {
+    switch (hs) {
+        case hls_state::initialized:    os << "initialized"; break;
+        case hls_state::loaded:         os << "loaded"; break;
+        case hls_state::running:        os << "running"; break;
+        case hls_state::stopped:        os << "stopped"; break;
+        case hls_state::terminated:     os << "terminated"; break;
+        case hls_state::exception:      os << "exception"; break;
+        default: break;
+    }
+    return os;
+}
+
 class frisc_hls {
 public:
     frisc_hls(size_t mem_size) : _state(hls_state::initialized), _mem_size(mem_size)
-        { _memory = new byte[mem_size]; memset(R, 0, sizeof R); PC = SR = 0; IIF = true; }
+        { _memory = new byte[mem_size]; }
     ~frisc_hls() { delete[] _memory; }
 
     void load_program(const char* filename);
@@ -48,11 +64,11 @@ public:
     void print_reg_state();
 
     byte get_byte(word address)
-        { return _memory[address]; }
+        { return _test_address(address) ? _memory[address] : 0; }
     half get_half(word address)
-        { half h = 0; for (int i = 0; i < 2; ++i) { h = (h << 8) | _memory[address + 1 - i]; } return h; }
+        { if (!_test_address(address)) return 0; half h = 0; for (int i = 0; i < 2; ++i) { h = (h << 8) | _memory[address + 1 - i]; } return h; }
     word get_word(word address)
-        { word w = 0; for (int i = 0; i < 4; ++i) { w = (w << 8) | _memory[address + 3 - i]; } return w; }
+        { if (!_test_address(address)) return 0; word w = 0; for (int i = 0; i < 4; ++i) { w = (w << 8) | _memory[address + 3 - i]; } return w; }
 
     void set_byte(word address, byte value)
         { _memory[address] = value; }
@@ -62,16 +78,17 @@ public:
         { for (int i = 0; i < 4; ++i) _memory[address + i] = (value & (0xFF << (8*i))) >> (8*i); }
 
     // Register definitions
-    word R[8];        // R0-R7 (General-purpose)
+    word R[8] = {0};  // R0-R7 (General-purpose)
     word &SP = R[7];  // R7 == SP (Stack Pointer)
-    word PC;          // Program Counter
-    word SR;          // Status Register
+    word PC = 0;      // Program Counter
+    word SR = 0;      // Status Register
 
-    bool IIF;           // Internal Interrupt Flag
+    bool IIF = false; // Internal Interrupt Flag
 private:
     void _execute_single();
     void _check_interrupt();
     void _do_interrupt();
+    bool _test_address(word address) { return address < _mem_size; }
 
     hls_state _state;
     size_t _mem_size;
@@ -98,7 +115,7 @@ void frisc_hls::load_program(const char* filename) {
 }
 
 bool frisc_hls::run(bool trace) {
-    if (_state != hls_state::loaded || _state != hls_state::stopped)
+    if (_state != hls_state::loaded && _state != hls_state::stopped)
         return false;
 
     _state = hls_state::running;
@@ -131,16 +148,16 @@ void frisc_hls::_execute_single() {
 
     // MOVE operation
     if (opcode == 0b00000) {
-        word &dest = R[(instr & (0b111 << 23)) >> 23];
+        word *dest = &R[(instr & (0b111 << 23)) >> 23];
         word src = fn ? sign_extend20(instr & 0x000FFFFF) : R[(instr & (0b111 << 17)) >> 17];
 
         bool ssr = instr & (1 << 21);
         bool dsr = instr & (1 << 20);
 
         if (ssr) src = SR;
-        if (dsr) dest = SR;
+        if (dsr) dest = &SR;
 
-        dest = src;
+        *dest = src;
 
     // Arithmetical-logical operation
     } else if (opcode >= 0b00001 && opcode <= 0b01100) {
@@ -148,7 +165,8 @@ void frisc_hls::_execute_single() {
         word src1 = R[(instr & (0b111 << 20)) >> 20];
         word src2 = fn ? sign_extend20(instr & 0x000FFFFF) : R[(instr & (0b111 << 17)) >> 17];
 
-        bool N = SR & 1, C = SR & 2, V = SR & 4, Z = SR & 8;
+        bool C = SR & 2;
+        bool nN = 0, nC = 0, nV = 0, nZ = 0;
 
         switch(opcode) {
             default: {
@@ -157,85 +175,79 @@ void frisc_hls::_execute_single() {
                 break;
             } case 0b00001: {  // OR
                 dest = src1 | src2;
-                C = V = 0;
                 break;
             } case 0b00010: {  // AND
                 dest = src1 & src2;
-                C = V = 0;
                 break;
             } case 0b00011: {  // XOR
                 dest = src1 ^ src2;
-                C = V = 0;
                 break;
             } case 0b00100: { // ADD
                 dest = src1 + src2;
                 auto cv = calc_add_flags(src1, src2);
-                C = cv.first; V = cv.second;
+                nC = cv.first; nV = cv.second;
                 break;
             } case 0b00101: {  // ADC
                 dest = src1 + src2 + C;
                 auto cv = calc_add_flags(src1, src2 + C);
-                C = cv.first; V = cv.second;
+                nC = cv.first; nV = cv.second;
                 break;
             } case 0b00110: {  // SUB
-                dest = src1 + ~src2;
-                auto cv = calc_add_flags(src1, ~src2);
-                C = cv.first; V = cv.second;
+                dest = src1 + ~src2 + 1;
+                auto cv = calc_add_flags(src1, ~src2 + 1);
+                nC = cv.first; nV = cv.second;
                 break;
             } case 0b00111: {  // SBC - TODO: Check validity of assumptions
-                dest = src1 + ~(src2 + C);
-                auto cv = calc_add_flags(src1, ~(src2 + C));
-                C = cv.first; V = cv.second;
+                dest = src1 + ~(src2 + C) + 1;
+                auto cv = calc_add_flags(src1, ~(src2 + C) + 1);
+                nC = cv.first; nV = cv.second;
                 break;
-            } case 0b01000: {  // ROTL - TODO: Implement
+            } case 0b01000: {
                 src2 &= 0x1F;  // (% 32)
-                int wrap_bits = 32 - src2;
-                word wrap_mask = (1 << wrap_bits) - 1;
-                // dest = (src1 << src2) | ();
-                // C =
-                V = 0;
+                word wrap_mask = ((1 << src2) - 1) << (32 - src2);
+                word wrap_bits = (src1 & wrap_mask) >> (32 - src2);
+                dest = (src1 << src2) | wrap_bits;
+                nC = wrap_bits & 1;
                 break;
-            } case 0b01001: {  // ROTR - TODO: Implement
+            } case 0b01001: {
                 src2 &= 0x1F;  // (% 32)
-                int wrap_bits = 32 - src2;
-                word wrap_mask = (1 << wrap_bits) - 1;
-
-                // C =
-                V = 0;
+                word wrap_mask = (1 << src2) - 1;
+                word wrap_bits = (src1 & wrap_mask);
+                dest = (src1 >> src2) | (wrap_bits << (32 - src2));
+                nC = wrap_bits & 1;
                 break;
             } case 0b01010: {  // SHL
                 dest = src1 << src2;
-                // C =
-                V = 0;
+                nC = src1 & (1 << (33 - src2));  // 1 << (32 - src2 + 1)
                 break;
             } case 0b01011: {  // SHR
                 dest = src1 >> src2;
-                V = 0;
+                nC = src1 & (1 << (src2 - 1));
                 break;
             } case 0b01100: {  // ASHR
                 dest = ((signed) src1) >> src2;
-                V = 0;
+                nC = src1 & (1 << (src2 - 1));
                 break;
             } case 0b01101: {  // CMP
                 unsigned res = src1 - src2;
                 auto cv = calc_add_flags(src1, ~src2);
-                C = cv.first; V = cv.second;
-                N = res & (1UL << 31);
-                Z = res == 0;
+                nC = cv.first; nV = cv.second;
+                nN = res & (1UL << 31);
+                nZ = res == 0;
                 break;
             }
         }
 
         if (opcode != 0b01101) {  // If not a CMP (CMP sets all flags on its own)
-            N = dest & (1UL << 31);
-            Z = dest == 0;
+            nN = dest & (1UL << 31);
+            nZ = dest == 0;
         }
 
         SR &= ~0b1111;  // Clear lower 4 bits
-        SR |= (Z << 3) | (V << 2) | (C << 1) | N;
+        SR |= (nZ << 3) | (nV << 2) | (nC << 1) | nN;
 
     // Memory operation
-    } else if (opcode >= 0b10010 && opcode <= 0b10111) {
+    } else if (opcode >= 0b10000 && opcode <= 0b10111) {
         unsigned &reg = R[(instr & (0b111 << 23)) >> 23];
         unsigned adr_reg = R[(instr & (0b111 << 20)) >> 20];
         unsigned addr = sign_extend20(instr & 0x000FFFFF) + (fn ? adr_reg : 0);
@@ -281,26 +293,40 @@ void frisc_hls::_execute_single() {
     } else if (opcode >= 0b11000 && opcode <= 0b11111) {
         unsigned cond = (instr & (0b1111 << 22)) >> 22;
 
-        switch (opcode) {
-            default:
-                fprintf(stderr, "Unknown operation: %04X!\n", opcode);
-                _state = hls_state::exception;
-                break;
-            case 0b11000:  // JP
-                //
-                break;
-            case 0b11001:  // CALL
-                //
-                break;
-            case 0b11010:  // JR
-                //
-                break;
-            case 0b11011:  // RET[I/N]
-                //
-                break;
-            case 0b11111:  // HALT
-                _state = hls_state::terminated;
-                break;
+        if (evaluate_condition(cond, SR & 8, SR & 4, SR & 2, SR & 1)) {
+            word addr = fn ? sign_extend20(instr & 0x000FFFFF) : R[(instr & (0b111 << 17)) >> 17];
+
+            switch (opcode) {
+                default:
+                    fprintf(stderr, "Unknown operation: %04X!\n", opcode);
+                    _state = hls_state::exception;
+                    break;
+                case 0b11000:  // JP
+                    PC = addr;
+                    break;
+                case 0b11001:  // CALL
+                    SP -= 4;
+                    set_word(SP, PC);
+                    PC = addr;
+                    break;
+                case 0b11010:  // JR
+                    PC += addr;
+                    break;
+                case 0b11011: { // RET[I/N]
+                    unsigned rt_code = instr & 0b11;
+
+                    PC = get_word(SP);
+                    SP += 4;
+
+                    if      (rt_code == 0b01 /* RETI */)  SR |= 16;  // GIE = 1
+                    else if (rt_code == 0b11 /* RETN */)  IIF = 1;
+
+                    break;
+                }
+                case 0b11111:  // HALT
+                    _state = hls_state::terminated;
+                    break;
+            }
         }
     }
 
@@ -309,7 +335,7 @@ void frisc_hls::_execute_single() {
 }
 
 int main(int, char* argv[]) {
-    frisc_hls simulator(1024);
+    frisc_hls simulator(4096);
     simulator.load_program(argv[1]);
     std::cout << simulator.run(true) << "\n";
 }
