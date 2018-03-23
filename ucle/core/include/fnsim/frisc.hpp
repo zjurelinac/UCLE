d40ea4699ed2eea6dc8cda960872e3d806bd6472
Zvonimir Jurelinac <zjurelinac@gmail.com>
2018-03-23 22:24:06 +0100

    Implemented large parts of FRISC fnsim

--- ucle/core/include/fnsim/frisc.hpp
+++ ucle/core/include/fnsim/frisc.hpp
@@ -9,22 +9,19 @@
 #include <fnsim/basic_devices.hpp>
 #include <fnsim/registers.hpp>
 #include <fnsim/simulator.hpp>
+#include <util/binary.hpp>
 
 namespace ucle::fnsim {
 
     struct frisc_register_file : register_file {
         std::array<reg<32>, 8> R;
         reg<32>& SP = R[7];
-
         reg<32> PC;
         reg<32> SR;  // TODO: A flags register!
 
-        reg<32> IR;
-
         void clear() override
         {
-            PC = SR = IR = 0;
-
+            PC = SR = 0;
             for (auto i = 0u; i < R.size(); ++i)
                 R[i] = 0;
         }
@@ -33,12 +30,23 @@ namespace ucle::fnsim {
     class frisc_simulator : public functional_simulator_impl<frisc_register_file, address_space<mapped_device_ptr>, memory> {
         using functional_simulator_impl<frisc_register_file, address_space<mapped_device_ptr>, memory>::functional_simulator_impl;
 
+        using cbu = util::const_bit_util<word_t>;
+        using unop = util::unop<word_t>;
+        using binop = util::binop<word_t>;
+
         protected:
             virtual address_t get_program_counter_() const override { return regs_.PC.get(); };
             virtual void set_program_counter_(address_t location) override { regs_.PC = location; }
 
             virtual status_t execute_single_() override;
 
+        private:
+            status_t execute_move_(word_t opcode, bool fn, const reg<32>& IR);
+            status_t execute_alu_(word_t opcode, bool fn, const reg<32>& IR);
+            status_t execute_mem_(word_t opcode, bool fn, const reg<32>& IR);
+            status_t execute_ctrl_(word_t opcode, bool fn, const reg<32>& IR);
+
+            constexpr bool eval_cond_(word_t cond) const;
     };
 
 }
--- ucle/core/src/fnsim/frisc.cpp
+++ ucle/core/src/fnsim/frisc.cpp
@@ -1,17 +1,229 @@
+#include <common/types.hpp>
 #include <fnsim/frisc.hpp>
 
 #include <iostream>
 
-ucle::status_t ucle::fnsim::frisc_simulator::execute_single_() {
-    if (regs_.PC > 20)
-        return error::invalid_state;
 
-    regs_.IR = read_word(regs_.PC);
-    std::cout << regs_.PC.get() << ":" << regs_.IR[{31, 27}] << "\n";
+ucle::status_t ucle::fnsim::frisc_simulator::execute_move_(word_t, bool fn, const reg<32>& IR)
+{
+    std::cout << "MOVE" << "\n";
+
+    auto dest = IR[20] ? &regs_.SR : &regs_.R[IR[{25, 23}]];
+    auto src = IR[21] ? regs_.SR.get() : (fn ? unop::sign_extend(IR[{19, 0}], 20) : regs_.R[IR[{19, 17}]].get());
+
+    *dest = src;
+    return success::ok;
+}
+
+ucle::status_t ucle::fnsim::frisc_simulator::execute_alu_(word_t opcode, bool fn, const reg<32>& IR)
+{
+    auto dest = &regs_.R[IR[{25, 23}]];
+    auto src1 = regs_.R[IR[{22, 20}]].get();
+    auto src2 = fn ? unop::sign_extend(IR[{19, 0}], 20) : regs_.R[IR[{19, 17}]].get();
+
+    switch (opcode) {
+        case 0b00001:
+            std::cout << "OR" << "\n";
+            // auto [res, new_flags] = binop::op_or(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b00010:
+            std::cout << "AND" << "\n";
+            // auto [res, new_flags] = binop::op_and(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b00011:
+            std::cout << "XOR" << "\n";
+            // auto [res, new_flags] = binop::op_xor(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b00100:
+            std::cout << "ADD" << "\n";
+            // auto [res, new_flags] = binop::op_add(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b00101:
+            std::cout << "ADC" << "\n";
+            // auto [res, new_flags] = binop::op_adc(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b00110:
+            std::cout << "SUB" << "\n";
+            // auto [res, new_flags] = binop::op_sub(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b00111:
+            std::cout << "SBC" << "\n";
+            // auto [res, new_flags] = binop::op_sbc(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b01000:
+            std::cout << "ROTL" << "\n";
+            // auto [res, new_flags] = binop::op_rtl(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b01001:
+            std::cout << "ROTR" << "\n";
+            // auto [res, new_flags] = binop::op_rtr(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b01010:
+            std::cout << "SHL" << "\n";
+            // auto [res, new_flags] = binop::op_shl(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b01011:
+            std::cout << "SHR" << "\n";
+            // auto [res, new_flags] = binop::op_shr(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b01100:
+            std::cout << "ASHR" << "\n";
+            // auto [res, new_flags] = binop::op_asr(src1, src2, old_flags);
+            // dest = res;
+            // flags = new_flags;
+            break;
+        case 0b01101:
+            std::cout << "CMP" << "\n";
+            // auto [_, new_flags] = binop::op_asr(src1, src2, old_flags);
+            // flags = new_flags;
+            break;
+        default:
+            return error::invalid_instruction;
+    }
+
+    return success::ok;
+}
+
+ucle::status_t ucle::fnsim::frisc_simulator::execute_mem_(word_t opcode, bool fn, const reg<32>& IR)
+{
+    auto& reg = regs_.R[IR[{25, 23}]];
+    auto addr = unop::sign_extend(IR[{19, 0}], 20) + (fn ? regs_.R[IR[{22, 20}]].get() : 0);
+
+    switch (opcode) {
+        case 0b10000:
+            std::cout << "POP" << "\n";
+            // reg = read_word(regs_.SP.get());
+            // regs_.SP += 4;
+            break;
+        case 0b10001:
+            std::cout << "PUSH" << "\n";
+            // regs_.SP -= 4;
+            // write_word(regs_.SP.get(), reg);
+            break;
+        case 0b10010:
+            std::cout << "LOADB" << "\n";
+            // reg = read_byte(addr);
+            break;
+        case 0b10011:
+            std::cout << "STOREB" << "\n";
+            // write_byte(addr, reg.get());  // TODO: get_byte?
+            break;
+        case 0b10100:
+            std::cout << "LOADH" << "\n";
+            // reg = read_half(addr);
+            break;
+        case 0b10101:
+            std::cout << "STOREH" << "\n";
+            // write_half(addr, reg.get());  // TODO: get_half?
+            break;
+        case 0b10110:
+            std::cout << "LOAD" << "\n";
+            // reg = read_word(addr);
+            break;
+        case 0b10111:
+            std::cout << "STORE" << "\n";
+            // write_word(addr, reg.get());
+            break;
+    }
+
+    return success::ok;
+}
+
+ucle::status_t ucle::fnsim::frisc_simulator::execute_ctrl_(word_t opcode, bool fn, const reg<32>& IR)
+{
+    if (!eval_cond_(IR[{25, 22}]))
+        return success::ok;
+
+    auto addr = fn ? unop::sign_extend(IR[{19, 0}], 20) : regs_.R[IR[{19, 17}]].get();
+
+    switch (opcode) {
+        case 0b11000:
+            std::cout << "JP" << "\n";
+            // regs_.PC = addr;
+            break;
+        case 0b11001:
+            std::cout << "CALL" << "\n";
+            // regs_.SP -= 4;
+            // write_word(regs_.SP.get(), regs_.PC.get())
+            // regs_.PC = addr;
+            break;
+        case 0b11010:
+            std::cout << "JR" << "\n";
+            // regs_.SP += addr;
+            break;
+        case 0b11011: {
+            std::cout << "RETX" << "\n";
+            // auto rtcode = IR[{1, 0}];
+            // regs_.PC = read_word(regs_.SP.get());
+            // regs_.SP += 4;
+            // if (rtcode == 0b01)      /* RETI */
+            //      /* set GIE = 1 */
+            // else if (rtcode == 0b11) /* RETN */
+            //      /* set IIF = 1 */
+            break;
+        }
+        case 0b11111:
+            std::cout << "HALT" << "\n";
+            terminate_();
+            break;
+        default:
+            return error::invalid_instruction;
+    }
+
+    return success::ok;
+}
+
+constexpr bool ucle::fnsim::frisc_simulator::eval_cond_(word_t) const
+{
+    return true;
+}
+
+ucle::status_t ucle::fnsim::frisc_simulator::execute_single_() {
+    reg<32> IR = read_word(address_t(regs_.PC));
+    std::cout << regs_.PC.get() << ": ";
+
+    auto opcode = IR[{31, 27}];
+    auto fn = IR[26];
+
+    status_t stat;
+    if (opcode == 0b00000) {
+        stat = execute_move_(opcode, fn, IR);
+    } else if (opcode >= 0b00001 && opcode <= 0b01101) {
+        stat = execute_alu_(opcode, fn, IR);
+    } else if (opcode >= 0b10000 && opcode <= 0b10111) {
+        stat = execute_mem_(opcode, fn, IR);
+    } else if (opcode >= 0b11000 && opcode <= 0b11111) {
+        stat = execute_ctrl_(opcode, fn, IR);
+    } else {
+        std::cout << "Unknown!" << "\n";
+        return error::invalid_instruction;
+    }
 
     regs_.PC += 4;
 
-    return success::ok;
+    return stat;
 }
 
 /* Test */
