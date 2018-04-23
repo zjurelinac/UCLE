c78737062e0318d157beba0940997963c403e416
Matija Čavrag <matija.cavrag@fer.hr>
2018-04-23 13:18:14 +0200

    Displaying files done. Needs more style.

--- ucle-ide/scripts/file_manage.js
+++ ucle-ide/scripts/file_manage.js
@@ -3,6 +3,7 @@ const ipcRenderer = require('electron').ipcRenderer;
 const fs = require('fs');
 const path = require('path');
 const mime = require('mime');
+const dirTree = require('directory-tree');
 
 class FileManager {
 
@@ -15,6 +16,13 @@ class FileManager {
 		ipcRenderer.on('save-as-file', (e) => this.saveAsFile());
 	}
 
+	getFileName(str) {
+		return str.split('\\').pop().split('/').pop();
+	}
+
+	getDirName(str) {
+		return str.match(/([^\/]*)\/*$/)[1];
+	}
 
 	openFile(file) {
 		let data = fs.readFileSync(file, 'utf-8');
@@ -52,32 +60,63 @@ class FileManager {
 		});
 	}
 
-	readFolder(filePath) {
+	walkThroughFiles(children, listID) {
+		let list = document.getElementById(listID);
+		for (var i = 0, len = children.length; i < len; i++) {
+			var child = children[i];
+			if(child.type == 'directory') {
+				let childListID = child.path + '-' + child.name;
+				list.innerHTML += '<li id="' + child.path + '" class="dir">' + child.name + '</li><ul id="' + childListID + '"></ul><br/>';
+				list.style.display = 'inherit';
+			} else {
+				list.innerHTML += '<li id="' + child.path + '" class="file">'+ child.name + '</li> <br/>';
+			}
+		}
+	}
+
+	readFolder(filePath, init = true) {
 		if(filePath === null || filePath === undefined) { 
 			filePath = './'
 		}
 
 		this.startPath = filePath;
 
-		fs.readdir(filePath, (err, files) => {
-			'use strict';
-			if (err) throw  err;
-			document.getElementById('listed-files').innerHTML = `<ul id="display-files"></ol>`;
-			for (let file of files) {
-				fs.stat(filePath + file, (err, stats) => {
-					let theID = `${filePath}${file}/`;
-					if (err) throw err;
-					if (stats.isDirectory()) {
-						document.getElementById('display-files').innerHTML += `<li id=${theID} class="dir"><i class="far fa-folder"></i> ${file}</li> <br/>`;
-					} else {
-						theID = theID.slice(0, -1);
-						document.getElementById('display-files').innerHTML += `<li id=${theID} class="file"><i class="far fa-file"></i>  ${file}</li> <br/>`;
-					}
+		var dirName = this.getDirName(filePath);
 
-					document.getElementById(theID).style.padding = "5px 5px 0px 0px";
-				});
+		let listID = filePath + '-' + dirName;
+
+		if(document.getElementById(listID) != null && document.getElementById(listID).innerHTML != '') {
+			if(document.getElementById(listID).style.display == 'none') {
+				document.getElementById(listID).style.display = 'inherit';
+			} else {
+				document.getElementById(listID).style.display = 'none';
 			}
-		});
+			return;
+		}
+		const tree = dirTree(filePath);
+
+		tree.path = dirName;
+
+		if(init) {
+			document.getElementById('openbtn').style.display = "none";
+
+			document.getElementById('listed-files').innerHTML += '<span id="' + filePath + '">' + dirName + '</span><ul id="' + listID + '"></ul>';
+
+			document.getElementById(filePath).addEventListener('click', function(e) {
+					if(document.getElementById(listID).style.display == "none") {
+						document.getElementById(listID).style.display = "inherit";
+					} else {
+						document.getElementById(listID).style.display = "none";
+					}
+			});
+
+			document.getElementById(filePath).addEventListener('mouseover', function(e) {
+					document.getElementById(filePath).style.cursor = "pointer";
+			});
+
+		}
+
+		this.walkThroughFiles(tree.children, listID);
 	}
 }
 
