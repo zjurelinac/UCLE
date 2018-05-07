533024385095cc7d75f5a97ada5b8c781a2f1635
Zvonimir Jurelinac <zjurelinac@gmail.com>
2018-05-07 12:52:38 +0200

    [util] Various bugfixes in simple shared ptr

--- ucle/core/include/util/simple_shared_ptr.hpp
+++ ucle/core/include/util/simple_shared_ptr.hpp
@@ -4,59 +4,59 @@
 #include <algorithm>
 #include <cstdio>  // TODO: Remove
 #include <memory>
+#include <type_traits>
 
 namespace ucle::util {
 
+    class ref_counter {
+        public:
+            using counter_type = long;
+
+            ref_counter() {}
+            ref_counter(const ref_counter& rc) : pn_ { rc.pn_ } {}
+
+            void swap(ref_counter& lhs) noexcept { std::swap(pn_, lhs.pn_); }
+            counter_type count() const noexcept { return pn_ ? *pn_ : 0; }
+
+            template <typename U>
+            void acquire(U* p)
+            {
+                if (!p) return;
+
+                if (!pn_) {
+                    try {
+                        pn_ = new counter_type { 0 };  // may throw std::bad_alloc
+                    } catch (std::bad_alloc&) {
+                        delete p;
+                        throw;  // rethrow the std::bad_alloc
+                    }
+                }
+                
+                ++(*pn_);
+            }
+
+            template<class U>
+            void release(U* p) noexcept
+            {
+                if (!pn_) return;
+
+                --(*pn_);
+
+                if (*pn_ == 0) {
+                    puts("deleting");
+                    delete p;
+                    delete pn_;
+                }
+                
+                pn_ = nullptr;
+            }
+
+        private:
+            counter_type* pn_ { nullptr };
+    };
+
     template <typename T>
     class simple_shared_ptr {
-        
-        class ref_counter {
-            public:
-                using counter_type = long;
-
-                ref_counter() {}
-                ref_counter(const ref_counter& rc) : pn_ { rc.pn_ } {}
-
-                void swap(ref_counter& lhs) noexcept { std::swap(pn_, lhs.pn_); }
-                counter_type count() const noexcept { return pn_ ? *pn_ : 0; }
-
-                template <typename U>
-                void acquire(U* p)
-                {
-                    if (!p) return;
-
-                    if (!pn_) {
-                        try {
-                            pn_ = new counter_type { 0 };  // may throw std::bad_alloc
-                        } catch (std::bad_alloc&) {
-                            delete p;
-                            throw;  // rethrow the std::bad_alloc
-                        }
-                    }
-                    
-                    ++(*pn_);
-                }
-
-                template<class U>
-                void release(U* p) noexcept
-                {
-                    if (!pn_) return;
-
-                    --(*pn_);
-
-                    if (*pn_ == 0) {
-                        puts("deleting");
-                        delete p;
-                        delete pn_;
-                    }
-                    
-                    pn_ = nullptr;
-                }
-
-            private:
-                counter_type* pn_ { nullptr };
-        };
-
         public:
             using element_type = T;
 
@@ -64,10 +64,8 @@ namespace ucle::util {
             explicit simple_shared_ptr(T* p) : pn_ {} { acquire_(p); }
             simple_shared_ptr(const simple_shared_ptr& ptr) noexcept : pn_ { ptr.pn_ } { acquire_(ptr.px_); }
 
-            template <typename U>
-            simple_shared_ptr(const simple_shared_ptr<U>& ptr) noexcept : pn_ { ptr.pn } { acquire_(static_cast<typename simple_shared_ptr<T>::element_type*>(ptr.px_)); }
-            template <typename U>
-            simple_shared_ptr(const simple_shared_ptr<U>& ptr, T* p) : pn_ { ptr.pn_ } { acquire_(p); }
+            template <typename U, typename = std::enable_if<std::is_convertible_v<U, T>>>
+            simple_shared_ptr(const simple_shared_ptr<U>& ptr) noexcept : pn_ { ptr.pn_ } { acquire_(static_cast<typename simple_shared_ptr<T>::element_type*>(ptr.px_)); }
 
             ~simple_shared_ptr() noexcept { release_(); }
 
@@ -89,6 +87,9 @@ namespace ucle::util {
             T* get() const noexcept { return px_; }
 
         private:
+            // template <typename U>
+            // simple_shared_ptr(const simple_shared_ptr<U>& ptr, T* p) : pn_ { ptr.pn_ } { acquire_(p); }
+
             void acquire_(T* p) { pn_.acquire(p); px_ = p; }
             void release_() noexcept { pn_.release(px_); px_ = nullptr; }
 
@@ -96,13 +97,13 @@ namespace ucle::util {
             ref_counter pn_;
 
             template <typename U>
-            friend class basic_shared_ptr;
-        
-        template<typename U, typename V>
-        friend bool operator==(const simple_shared_ptr<U>& l, const simple_shared_ptr<V>& r) noexcept { return l.get() == r.get(); }
-        template<typename U, typename V>
-        friend bool operator!=(const simple_shared_ptr<U>& l, const simple_shared_ptr<V>& r) noexcept { return l.get() != r.get(); }
+            friend class simple_shared_ptr;        
     };
+
+    template<typename U, typename V>
+    inline bool operator==(const simple_shared_ptr<U>& l, const simple_shared_ptr<V>& r) noexcept { return l.get() == r.get(); }
+    template<typename U, typename V>
+    inline bool operator!=(const simple_shared_ptr<U>& l, const simple_shared_ptr<V>& r) noexcept { return l.get() != r.get(); }
 
     // template<typename T, typename U>
     // simple_shared_ptr<T> static_pointer_cast(const simple_shared_ptr<U>& ptr) noexcept
