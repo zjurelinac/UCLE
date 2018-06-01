ef6282965616e58dd39ac31477c8c748b3276c6e
Zvonimir Jurelinac <zjurelinac@gmail.com>
2018-06-01 16:57:38 +0200

    [asm::frisc] Machine code generation completed

--- ucle/core/src/asm/processors/frisc.cpp
+++ ucle/core/src/asm/processors/frisc.cpp
@@ -1,5 +1,7 @@
 #include <asm/processors/frisc.hpp>
 
+#include <common/structures.hpp>
+
 #include <libs/fmt/format.h>
 
 #include <util/string.hpp>
@@ -7,6 +9,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <unordered_map>
 
 using namespace ucle;
 using namespace ucle::asr;
@@ -25,15 +28,40 @@ namespace dbg {
 }
 
 namespace detail {
+    static inline bool fits_into_nbits(unsigned num, int n)
+    {
+        return num >> n == 0;
+    }
+
+    static inline bool fits_ext_into_nbits(int num, int n)
+    {
+        auto top_bits = num >> n;
+        return top_bits == 0 || top_bits == -1;
+    }
+
+    static inline void fill_bytes(int value, unsigned byte_no, small_byte_vector& bv)
+    {
+        using cbu = util::const_bin_util<int>;
+
+        for (auto i = 0u; i < byte_no; ++i)
+            bv.push_back(cbu::nth_byte_of(value, i));
+    }
+
+    static inline word_t flush_bytes(small_byte_vector& bv)
+    {
+        word_t value = 0;
+        for (int i = bv.size() - 1; i >= 0; --i)
+            value = value << 8 | bv[i];
+        bv.clear();
+        return value;
+    }
+
     static int parse_num_const(const parse_details& num_const)
     {
         if (num_const == "signed_num_const") {
-            auto sign = num_const.has("sign") && num_const["sign"] == "-" ? -1 : 1;
+            auto sign = num_const.has("sign") && num_const["sign"].contents == "-" ? -1 : 1;
             return parse_num_const(num_const["num_const"]) * sign;
         }
-
-        if (num_const != "num_const")
-            throw parse_error("Cannot parse num_const - incorrect symbol type.");
 
         long long value = 0;
 
@@ -51,7 +79,94 @@ namespace detail {
         return value;
     }
 
-    static int data_instr_size(std::string_view dat_opcode)
+    static int parse_gp_reg(const parse_details& gp_reg)
+    {
+        static std::unordered_map<std::string_view, int> gp_regs = {
+            { "R0", 0b000 }, { "R1", 0b001 }, { "R2", 0b010 },
+            { "R3", 0b011 }, { "R4", 0b100 }, { "R5", 0b101 },
+            { "R6", 0b110 }, { "R7", 0b111 }, { "SP", 0b111 }
+        };
+
+        return gp_regs[gp_reg.contents];
+    }
+
+    static int parse_label(const parse_details& label, const label_table& labels)
+    {
+        if (!labels.count(label.contents))
+            throw logical_error(fmt::format("Unknown label: {}", label.contents));
+        return labels.at(label.contents);
+    }
+
+    static int parse_immediate(const parse_details& imm, const label_table& labels)
+    {
+        if (imm == "label")
+            return parse_label(imm, labels);
+
+        auto val = parse_num_const(imm);
+
+        if (!fits_ext_into_nbits(val, 20))
+            throw logical_error(fmt::format("Constant {} cannot fit into sign-extended 20 immediate values.", imm.contents));
+
+        return val;
+    }
+
+    static int parse_opcode(const parse_details& opcode)
+    {
+        static std::unordered_map<std::string_view, int> opcodes = {
+            { "MOVE", 0b00000 }, { "OR", 0b00001 }, { "AND", 0b00010 }, { "XOR", 0b00011 },
+            { "ADD", 0b00100 }, { "ADC", 0b00101 }, { "SUB", 0b00110 }, { "SBC", 0b00111 },
+            { "ROTL", 0b01000 }, { "ROTR", 0b01001 }, { "SHL", 0b01010 }, { "SHR", 0b01011 },
+            { "ASHR", 0b01100 }, { "CMP", 0b01101 }, { "POP", 0b10000 }, { "PUSH", 0b10001 },
+            { "LOADB", 0b10010 }, { "STOREB", 0b10011 }, { "LOADH", 0b10100 }, { "STOREH", 0b10101 },
+            { "LOAD", 0b10110 }, { "STORE", 0b10111 }, { "JP", 0b11000 }, { "CALL", 0b11001 },
+            { "JR", 0b11010 }, { "RET", 0b11011 }, { "RETI", 0b11011 }, { "RETN", 0b11011 },
+            { "HALT", 0b11111 }
+        };
+
+        return opcodes[opcode.contents];
+    }
+
+    static int parse_reg_off(const parse_details& reg_off)
+    {
+        if (reg_off == "gp_reg")
+            return parse_gp_reg(reg_off) << 20;
+
+        word_t reg = parse_gp_reg(reg_off[0]);
+        auto sign = reg_off["sign"].contents == "-" ? -1 : 1;
+        word_t imm = parse_num_const(reg_off[2]);
+
+        if (!fits_ext_into_nbits(imm, 20))
+            throw logical_error(fmt::format("Constant {} cannot fit into sign-extended 20 immediate values.", reg_off[2].contents));
+
+        return reg << 20 | (imm * sign & 0xFFFFF);
+    }
+
+    static int parse_condition(const parse_details& condition)
+    {
+        static std::unordered_map<std::string_view, int> conditions = {
+            { "N", 0b0001 }, { "M", 0b0001 }, { "NN", 0b0010 }, { "P", 0b0010 },
+            { "C", 0b0011 }, { "UGE", 0b0011 }, { "NC", 0b0100 }, { "ULT", 0b0100 },
+            { "V", 0b0101 }, { "NV", 0b0110 }, { "Z", 0b0111 }, { "EQ", 0b0111 },
+            { "NZ", 0b1000 }, { "NE", 0b1000 }, { "ULE", 0b1001 }, { "UGT", 0b1010 },
+            { "SLT", 0b1011 }, { "SLE", 0b1100 }, { "SGE", 0b1101 }, { "SGT", 0b1110 }
+        };
+
+        if (condition.empty())
+            return 0;
+
+        return conditions[condition.contents.substr(1)];
+    }
+
+    static int parse_retcode(const parse_details& opcode)
+    {
+        static std::unordered_map<std::string_view, int> retcodes = {
+            { "RET", 0b00 }, { "RETI", 0b01 }, { "RETN", 0b11 }, { "HALT", 0b00 }
+        };
+
+        return retcodes[opcode.contents];
+    }
+
+    static inline int data_instr_size(std::string_view dat_opcode)
     {
         if (util::iequals(dat_opcode, "DW")) return 4;
         if (util::iequals(dat_opcode, "DH")) return 2;
@@ -62,22 +177,92 @@ namespace detail {
 
     static word_t parse_alu_instr(const parse_details& instr, const label_table& labels)
     {
-        return 0;
+        word_t opcode = parse_opcode(instr["opcode"]);
+        word_t fn = instr[2] != "gp_reg";
+        word_t src1 = parse_gp_reg(instr[1]);
+        word_t src2 = fn ? parse_immediate(instr[2], labels) : parse_gp_reg(instr[2]) << 17;
+        word_t dest = parse_gp_reg(instr[3]);
+
+        return opcode << 27 | fn << 26 | dest << 23 | src1 << 20 | (src2 & 0xFFFFF);
     }
 
-    static word_t parse_cmp_instr(const parse_details& instr, const label_table& labels) { return 0; }
+    static word_t parse_cmp_instr(const parse_details& instr, const label_table& labels)
+    {
+        word_t opcode = parse_opcode(instr["opcode"]);
+        word_t fn = instr[2] != "gp_reg";
+        word_t src1 = parse_gp_reg(instr[1]);
+        word_t src2 = fn ? parse_immediate(instr[2], labels) : parse_gp_reg(instr[2]) << 17;
 
-    static word_t parse_mov_instr(const parse_details& instr, const label_table& labels) { return 0; }
+        return opcode << 27 | fn << 26 | src1 << 20 | (src2 & 0xFFFFF);
+    }
 
-    static word_t parse_mem_instr(const parse_details& instr, const label_table& labels) { return 0; }
+    static word_t parse_mov_instr(const parse_details& instr, const label_table& labels)
+    {
+        word_t opcode = parse_opcode(instr["opcode"]);
+        word_t sr_src = instr[1] == "sr_reg";
+        word_t sr_dst = instr[2] == "sr_reg";
+        word_t fn = !sr_src && instr[1] != "gp_reg";
+        word_t src = fn ? parse_immediate(instr[1], labels) : (sr_src ? 0 : parse_gp_reg(instr[1]) << 17);
+        word_t dst = (sr_dst ? 0 : parse_gp_reg(instr[2]));
 
-    static word_t parse_stk_instr(const parse_details& instr, const label_table& labels) { return 0; }
+        return opcode << 27 | fn << 26 | dst << 23 | sr_src << 21 | sr_dst << 20 | (src & 0xFFFFF);
+    }
 
-    static word_t parse_jmp_instr(const parse_details& instr, const label_table& labels) { return 0; }
+    static word_t parse_mem_instr(const parse_details& instr, const label_table& labels) {
+        word_t opcode = parse_opcode(instr["opcode"]);
+        word_t fn = instr[2] == "gp_reg" || instr[2] == "reg_off";
+        word_t reg = parse_gp_reg(instr[1]);
+        word_t addr = fn ? parse_reg_off(instr[2]) : parse_immediate(instr[2], labels);
 
-    static word_t parse_ret_instr(const parse_details& instr, const label_table& labels) { return 0; }
+        return opcode << 27 | fn << 26 | reg << 23 | (addr & 0x7FFFFF);
+    }
 
-    static void parse_dat_instr(const parse_details& instr, address32_t address, second_pass_result& mcode) {  }
+    static word_t parse_stk_instr(const parse_details& instr, const label_table&)
+    {
+        word_t opcode = parse_opcode(instr["opcode"]);
+        word_t reg = parse_gp_reg(instr[1]);
+
+        return opcode << 27 | reg << 23;
+    }
+
+    static word_t parse_jmp_instr(const parse_details& instr, const label_table& labels)
+    {
+        word_t opcode = parse_opcode(instr["opcode"]);
+        word_t condition = parse_condition(instr["condition"]);
+        word_t fn = instr[2] != "gp_reg";
+        word_t addr = fn ? parse_immediate(instr[2], labels) : parse_gp_reg(instr[2]) << 17;
+
+        return opcode << 27 | fn << 26 | condition << 22 | (addr & 0xFFFFF);
+    }
+
+    static word_t parse_ret_instr(const parse_details& instr, const label_table&)
+    {
+        word_t opcode = parse_opcode(instr["opcode"]);
+        word_t condition = parse_condition(instr["condition"]);
+        word_t retcode = parse_retcode(instr["opcode"]);
+
+        return opcode << 27 | condition << 22 | retcode;
+    }
+
+    static void parse_dat_instr(const parse_details& instr, address32_t address, second_pass_result& mcode)
+    {
+        auto data_size = data_instr_size(instr["opcode"].contents);
+        auto data_count = instr["def_list"].children.size();
+        auto& data_values = instr["def_list"].children;
+
+        small_byte_vector bytes;
+        for (auto i = 0u; i < data_count; ++i) {
+            auto value = parse_num_const(data_values[i]);
+            fill_bytes(value, data_size, bytes);
+
+            if (bytes.size() == 4) {
+                mcode[address] = flush_bytes(bytes);
+                address += 4;
+            }
+        }
+
+        mcode[address] = flush_bytes(bytes);
+    }
 }
 
 std::string frisc_assembler::read_file_(std::string filename)
@@ -125,6 +310,7 @@ first_pass_result frisc_assembler::first_pass_(const std::vector<parse_details>&
 
         if (instr_class[0] == "equ_instr") {
             labels[line["line_label"].contents] = detail::parse_num_const(instr_class[0]["signed_num_const"]);
+            continue;
         } else if (instr_class[0] == "org_instr") {
             address32_t new_addr = detail::parse_num_const(instr_class[0]["signed_num_const"]);
 
@@ -135,8 +321,10 @@ first_pass_result frisc_assembler::first_pass_(const std::vector<parse_details>&
                 throw logical_error(fmt::format("Impossible origin (ORG {}), lesser than current address (= {}).", new_addr, current_addr));
 
             current_addr = new_addr;
-            continue;
         }
+
+        if (!line["line_label"].empty())
+            labels[line["line_label"].contents] = current_addr;
 
         if (instr_class == "reg_instr") {
             instrs.push_back({ current_addr, instr_class[0] });
@@ -149,7 +337,7 @@ first_pass_result frisc_assembler::first_pass_(const std::vector<parse_details>&
 
             auto& dat_instr = instr_class[0];
             auto& def_list = dat_instr["def_list"];
-            auto data_size = detail::data_instr_size(dat_instr["dat_opcode"].contents);
+            auto data_size = detail::data_instr_size(dat_instr["opcode"].contents);
             auto data_count = def_list.children.size();
 
             current_addr += cbu::address_rounded(data_count * data_size + sizeof(word_t) - 1, sizeof(word_t));
@@ -198,16 +386,15 @@ void frisc_assembler::assemble(std::string filename)
     try {
         auto contents = read_file_(filename);
         auto parsed_lines = parse_lines_(contents);
-        auto [addressed_lines, labels] = first_pass_(parsed_lines);
-        auto assemble_results = second_pass_(addressed_lines, labels);
+        auto [instr_lines, labels] = first_pass_(parsed_lines);
 
-        // for (const auto& al : addressed_lines)
-        //     fmt::print("@{} :: {}\n", al.address, al.instr.contents);
+        for (const auto [label, address] : labels)
+            fmt::print("{} => {:08x}\n", label, address);
 
-        // for (const auto [label, addr] : labels)
-        //     fmt::print("* {} => {}\n", label, addr);
+        auto assemble_results = second_pass_(instr_lines, labels);
 
-        // Second pass - actual assembling
+        for (const auto [address, mcode] : assemble_results)
+            fmt::print("@{} :: {:08X} == {:032b}\n", address, mcode, mcode);
 
     } catch (std::exception& e) {
         fmt::print_colored(stderr, fmt::RED, "{}\n", e.what());
@@ -297,18 +484,19 @@ void frisc_assembler::init_parser_()
     auto def_list = def_item + N >= "def_list";
 
     auto alu_opcode = ilit("ADD") / ilit("ADC") / ilit("SUB") / ilit("SBC") / ilit("AND") / ilit("OR")  / ilit("XOR") /
-                      ilit("SHL") / ilit("SHR") / ilit("ASHR") / ilit("ROTL") / ilit("ROTR")                         >= "alu_opcode";
-    auto cmp_opcode = ilit("CMP")                                                                                    >= "cmp_opcode";
-    auto mov_opcode = ilit("MOVE")                                                                                   >= "mov_opcode";
-    auto mem_opcode = ilit("LOADH") / ilit("LOADB") / ilit("LOAD") / ilit("STOREH") / ilit("STOREB") / ilit("STORE") >= "mem_opcode";
-    auto stk_opcode = ilit("PUSH") / ilit("POP")                                                                     >= "stk_opcode";
-    auto jmp_opcode = ilit("JP") / ilit("JR") / ilit("CALL")                                                         >= "jmp_opcode";
-    auto ret_opcode = ilit("RETI") / ilit("RETN") / ilit("RET") / ilit("HALT")                                       >= "ret_opcode";
+                      ilit("SHL") / ilit("SHR") / ilit("ASHR") / ilit("ROTL") / ilit("ROTR")                         >= "opcode";
+    auto cmp_opcode = ilit("CMP")                                                                                    >= "opcode";
+    auto mov_opcode = ilit("MOVE")                                                                                   >= "opcode";
+    auto mem_opcode = ilit("LOADH") / ilit("LOADB") / ilit("LOAD") / ilit("STOREH") / ilit("STOREB") / ilit("STORE") >= "opcode";
+    auto stk_opcode = ilit("PUSH") / ilit("POP")                                                                     >= "opcode";
+    auto jmp_opcode = ilit("JP") / ilit("JR") / ilit("CALL")                                                         >= "opcode";
+    auto ret_opcode = ilit("RETI") / ilit("RETN") / ilit("RET") / ilit("HALT")                                       >= "opcode";
 
-    auto org_opcode = ilit("ORG")                          >= "org_opcode";
-    auto equ_opcode = ilit("EQU")                          >= "equ_opcode";
-    auto dsp_opcode = ilit("DS")                           >= "dsp_opcode";
-    auto dat_opcode = ilit("DW") / ilit("DH") / ilit("DB") >= "dat_opcode";
+    auto org_opcode = ilit("ORG")                          >= "opcode";
+    auto equ_opcode = ilit("EQU")                          >= "opcode";
+    auto dsp_opcode = ilit("DS")                           >= "opcode";
+    auto dat_opcode = ilit("DW") / ilit("DH") / ilit("DB") >= "opcode";
+    auto end_opcode = ilit("END")                          >= "opcode";
 
     auto alu_instr = alu_opcode >> spaces >> gp_reg >> cm_sep >> alu_operand >> cm_sep >> gp_reg >= "alu_instr";
     auto cmp_instr = cmp_opcode >> spaces >> gp_reg >> cm_sep >> alu_operand                     >= "cmp_instr";
@@ -322,9 +510,10 @@ void frisc_assembler::init_parser_()
     auto org_instr = org_opcode >> spaces >> immediate >= "org_instr";
     auto dsp_instr = dsp_opcode >> spaces >> immediate >= "dsp_instr";
     auto dat_instr = dat_opcode >> spaces >> def_list  >= "dat_instr";
+    auto end_instr = end_opcode                        >= "end_instr";
 
-    auto reg_instr = alu_instr / mem_instr / mov_instr / stk_instr / jmp_instr / ret_instr >= "reg_instr";
-    auto psd_instr = equ_instr / org_instr / dsp_instr / dat_instr                         >= "psd_instr";
+    auto reg_instr = alu_instr / cmp_instr / mem_instr / mov_instr / stk_instr / jmp_instr / ret_instr >= "reg_instr";
+    auto psd_instr = equ_instr / org_instr / dsp_instr / dat_instr / end_instr                         >= "psd_instr";
     auto any_instr = psd_instr / reg_instr;
 
     auto line_label   = label / eps()                            >= "line_label";
--- ucle/core/tests/asm/test_basic.asm
+++ ucle/core/tests/asm/test_basic.asm
@@ -1,16 +1,33 @@
-GPIO_DR EQU 0FFFFF100
+GPIO_DR EQU 0FFF10000
 
         ORG 0
-        MOVE 1000, SP
+        ; ADD R2, GPIO_DR, R2
+        ; XOR R3, R4, R5
+        ; CMP R0, 7
+        ; MOVE R2, R3
+        ; MOVE 0FFFC, SP
+        ; MOVE SR, R1
+        ; MOVE R1, SR
+        ; LOAD R2, (LABEL1)
+        ; LOADB R3, (R1)
+        ; STORE R4, (SP-8)
+        ; PUSH R1
+        ; POP SP
         JP MAIN
+        JP_ULE MAIN
+        JR_SGT MAIN
+        JP (R2)
 
-        ORG %D 100
+        DW 0ABCDEFFE
+LABEL1  DB 5, 6, 7, 8, 9, %D 10, %D 11, %D 12, 0D, 0E, 0F
+
+        ; ORG %D 100
 MAIN
         HALT
 
-        DS %D 40
-        DW 5, 6, 7
-        DB 1, 2, 3
-        DH 7, 8, 9, 10, 11, 12
-        DW 1
+        ; DS %D 40
+        ; DW 5, 6, 7
+        ; DB 1, 2, 3
+        ; DH 7, 8, 9, 10, 11, 12
+        ; DW 1
         ; stuff
