529a28ab1b7654d411f80499d32120cdfa703eb2
Zvonimir Jurelinac <zjurelinac@gmail.com>
2018-06-04 08:20:43 +0200

    [util::fs] Added file open and close functions

--- ucle/core/include/util/filesystem.hpp
+++ ucle/core/include/util/filesystem.hpp
@@ -1,9 +1,32 @@
 #ifndef _UCLE_CORE_UTIL_FILESYSTEM_HPP_
 #define _UCLE_CORE_UTIL_FILESYSTEM_HPP_
 
+#include <common/types.hpp>
+
+#include <libs/fmt/format.h>
+
 #include <util/string_view.hpp>
 
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+
 namespace ucle::util {
+
+    class filesystem_error : public base_exception { using base_exception::base_exception; };
+
+    auto open_file(const char* filename, const char* mode)
+    {
+        auto handle = fopen(filename, mode);
+        if (!handle)
+            throw filesystem_error(fmt::format("Error, cannot open file `{}`: {}.", filename, strerror(errno)));
+        return handle;
+    }
+
+    void close_file(FILE* handle)
+    {
+        fclose(handle);
+    }
 
     std::string strip_extension(std::string filename)
     {
