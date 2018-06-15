f04332c4fe30d35b026d31d97e5befffb969d14e
Zvonimir Jurelinac <zjurelinac@gmail.com>
2018-06-15 19:55:19 +0200

    [util::json] Added JSON utility function header

---
+++ ucle/core/include/util/json.hpp
@@ -0,0 +1,32 @@
+#ifndef _UCLE_CORE_UTIL_JSON_HPP_
+#define _UCLE_CORE_UTIL_JSON_HPP_
+
+#include <common/types.hpp>
+
+#include <libs/fmt/format.h>
+#include <libs/nlohmann/json.hpp>
+
+namespace ucle::util {
+
+    class json_error : public base_exception { using base_exception::base_exception; };
+
+    auto get(nlohmann::json j, std::string key)
+    {
+        if (!j.count(key))
+            throw json_error(fmt::format("Key '{}' missing from JSON object", key));
+
+        return j[key];
+    }
+
+    template <typename T>
+    T get(nlohmann::json j, std::string key, T default_val)
+    {
+        if (!j.count(key))
+            return default_val;
+
+        return j[key];
+    }
+
+}
+
+#endif  /* _UCLE_CORE_UTIL_JSON_HPP_ */
