5d6d7510cb8a367062b1a493ed5477d37d14753f
Zvonimir Jurelinac <zjurelinac@gmail.com>
2018-06-17 15:24:04 +0200

    [fnsim::cli] Major rewrite in progress (JSON-based config) - basic simulation running implemented

--- ucle/core/include/fnsim/cli.hpp
+++ ucle/core/include/fnsim/cli.hpp
@@ -4,9 +4,15 @@
 #include <fnsim/base.hpp>
 
 #include <fnsim/checker.hpp>
+#include <fnsim/exceptions.hpp>
 #include <fnsim/simulation.hpp>
 
 #include <fnsim/processors/frisc.hpp>
+
+#include <libs/nlohmann/json.hpp>
+
+#include <util/json.hpp>
+#include <util/string.hpp>
 
 #include <functional>
 #include <set>
@@ -17,33 +23,72 @@
 namespace ucle::fnsim::cli {
 
     template <unsigned N>
-    using fnsim_factory = std::function<functional_processor_simulator_ptr<N>(fnsim::processor_config)>;
+    using proc_factory_type = std::function<functional_processor_simulator_ptr<N>(fnsim::processor_config)>;
 
-    std::set<std::string> factory_options = {"frisc"};
-    std::unordered_map<std::string, fnsim_factory<32>> factory_32 = {
-        {"frisc", &frisc::make_frisc_simulator}
+    template <unsigned N> std::unordered_map<std::string, proc_factory_type<N>> proc_factory;
+    template <> std::unordered_map<std::string, proc_factory_type<32>> proc_factory<32> = {
+        { "frisc", &frisc::make_frisc_simulator }
     };
+    template <> std::unordered_map<std::string, proc_factory_type<64>> proc_factory<64> = {};
 
-    struct config {
-        // Processor options
-        std::string simulator_name;
-        uint32_t fnsim_mem_size = 4096;
 
-        // Source options
-        std::string pfile;
+    inline auto parse_config(nlohmann::json config)
+    {
+        auto sys_config = util::get(config, "system_options");
+        processor_config cfg { util::get(sys_config, "memory_size") };
+        return cfg;
+    }
 
-        // Runtime options
-        bool run_json = false;
-        bool run_interactive = false;
-        bool run_checker = false;
-        bool print_reg_info = false;
-        bool print_exec_info = false;
+    template <typename FunctionalSimulation>
+    inline FunctionalSimulation build_fnsim(nlohmann::json config)
+    {
+        std::string arch = util::lowercase(util::get(config, "architecture"));
 
-        // Checker options
-        std::vector<std::string> checks;
-        int verbosity;
+        if (!proc_factory<FunctionalSimulation::arch_width>.count(arch))
+            throw incorrect_call(fmt::format("Unknown architecture '{}' for given architecture width", arch));
+
+        return FunctionalSimulation { proc_factory<FunctionalSimulation::arch_width>[arch](parse_config(config)) };
+    }
+
+    template <unsigned N>
+    inline void run_basic_helper(std::string program_path, nlohmann::json config)
+    {
+        auto sim_options = util::get(config, "simulation_options");
+        auto fnsim = build_fnsim<functional_simulation<N, false, false, false, true>>(config);
+        fnsim.load_pfile(program_path);
+        fnsim.run();
+
+        if (util::get(sim_options, "print_registers", false))
+            print_reg_info(fnsim.get_reg_info());
+    }
+
+    void run_basic(std::string program_path, nlohmann::json config)
+    {
+        int width = util::get(config, "width", 32);
+        switch (width) {
+            case 32:
+                return run_basic_helper<32>(program_path, config);
+            case 64:
+                return run_basic_helper<64>(program_path, config);
+            default:
+                throw incorrect_call(fmt::format("Architecture width of {} is not supported.", width));
+        }
+    }
+
+    // void run_interactive_text(std::string program_path, nlohmann::json config) {}
+    // void run_interactive_json(std::string program_path, nlohmann::json config) {}
+    // void run_statistics(std::string program_path, nlohmann::json config) {}
+    // void run_testing(std::string program_path, nlohmann::json config) {}
+
+    using runner_type = std::function<void(std::string, nlohmann::json)>;
+
+    std::unordered_map<std::string, runner_type> runners = {
+        { "basic", &run_basic }
+        // { "interactive", &run_interactive_text },
+        // { "interactive_json", &run_interactive_json },
+        // { "statistics", &run_statistics },
+        // { "testing", &run_testing }
     };
-
 }
 
 #endif  /* _UCLE_CORE_FNSIM_CLI_HPP_ */
--- ucle/core/src/fnsim/cli.cpp
+++ ucle/core/src/fnsim/cli.cpp
@@ -1,75 +1,105 @@
 #include <fnsim/cli.hpp>
 
-#include <fnsim/simulations/interactive_text.hpp>
-#include <fnsim/simulations/interactive_json.hpp>
+// #include <fnsim/simulations/interactive_text.hpp>
+// #include <fnsim/simulations/interactive_json.hpp>
 
 #include <libs/cli/cli11.hpp>
 #include <libs/fmt/format.h>
 #include <libs/nlohmann/json.hpp>
 
+#include <util/json.hpp>
+#include <util/string.hpp>
+
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
 #include <memory>
 
 /*
-    fmt colors { BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE };
-
-    Color codes:
-        - YELLOW - important events
-        - GREEN - success
-        - RED - errors
-        - BLUE - info
-        - CYAN - regular highlight
+    Simulator config format:
+    {
+        "architecture": string,
+        "width": int,
+        "simulation_type": string,
+        "simulation_options": {
+            ["checks": [ string ]],
+            ["print_registers": bool],
+            ["verbosity": int],
+            ...
+        },
+        "system_options": {
+            "memory_size": int,
+            "devices": [
+                {
+                    "device_type": "frisc_gpio_led",
+                    "device_name": "gpio_led",
+                    "memory_"
+                }
+            ]
+        }
+    }
 */
 
 int main(int argc, char* argv[]) {
     using namespace ucle;
     using namespace ucle::fnsim;
     using namespace ucle::fnsim::cli;
+    using namespace nlohmann;
 
     CLI::App app {"Functional simulator CLI"};
 
-    fnsim::cli::config cfg {};
+    std::string program_path, config_path;
 
-    app.add_set_ignore_case("simulator", cfg.simulator_name, factory_options, "Processor simulator to use for simulation")->required();
-    app.add_option("pfile", cfg.pfile, "A path to the machine-code .p file to be executed")->required()->check(CLI::ExistingFile);
+    app.add_option(
+        "program_file", program_path,
+        "A path to the file containing machine code which should be executed in the simulation"
+    )->required()->check(CLI::ExistingFile);
 
-    app.add_option("-m,--memory_size", cfg.fnsim_mem_size, "Internal memory size for the processor in the simulation", true);
-
-    app.add_flag("-i,--run-interactive", cfg.run_interactive, "Run the simulation interactively");
-    app.add_flag("-j,--run-json", cfg.run_json, "Run a JSON-controlled simulation");
-    app.add_flag("-r,--print-reg-info", cfg.print_reg_info, "Print register info after simulation run");
-    app.add_flag("-x,--print-exec-info", cfg.print_exec_info, "Print execution info after simulation run");
-
-    auto checker_cmd = app.add_subcommand("check", "Simulation results checker");
-    checker_cmd->set_callback([&cfg]() { cfg.run_checker = true; });
-    checker_cmd->add_option("check", cfg.checks, "A list of checks to perform once simulation finishes")->required();
-    auto verbose_callback = [&cfg](std::size_t count) { cfg.verbosity = count; };
-    checker_cmd->add_flag("-v,--verbose-output", verbose_callback, "Controls how verbose will the checker output be");
+    app.add_option(
+        "config_file", config_path,
+        "A path to the file containing simulator configuration options"
+    )->required()->check(CLI::ExistingFile);
 
     CLI11_PARSE(app, argc, argv);
 
-    processor_config proc_cfg {cfg.fnsim_mem_size};
+    try {
+        std::ifstream config_file { config_path };
+        json config = json::parse(config_file);
 
-    if (cfg.run_interactive) {
-        functional_simulation<> sim {factory_32[cfg.simulator_name](proc_cfg)};
-        run_interactive_text_simulation(sim, cfg);
-    } else if (cfg.run_json) {
-        functional_simulation<> sim {factory_32[cfg.simulator_name](proc_cfg)};
-        run_interactive_json_simulation(sim, cfg);
-    } else {
-        functional_simulation<false, false, false, true> sim {factory_32[cfg.simulator_name](proc_cfg)};
-        sim.load_pfile(cfg.pfile);
-        sim.run();
+        std::string simulation_type = util::get(config, "simulation_type");
 
-        if (cfg.print_reg_info)
-            print_reg_info(sim.get_reg_info());
+        runners[simulation_type](program_path, config);
 
-        if (cfg.print_exec_info)
-            print_exec_info(sim.get_exec_info());
-
-        if (cfg.run_checker) {
-            checker chk {sim, cfg.pfile, cfg.verbosity};
-            chk.run(cfg.checks);
-        }
+    } catch (std::exception& e) {
+        fmt::print_colored(stderr, fmt::RED, "Error: {}\n", e.what());
     }
+
+    // 1. DETERMINE SIMULATION TYPE
+    // 2. CONSTRUCT FUNCTIONAL_SIMULATION
+    // 3. ADD DEVICES
+    // 4. RUN SIMULATION
+    // 5. PROCESS & OUTPUT RESULTS
+
+    // if (cfg.run_interactive) {
+    //     functional_simulation<> sim {factory_32[cfg.simulator_name](proc_cfg)};
+    //     run_interactive_text_simulation(sim, cfg);
+    // } else if (cfg.run_json) {
+    //     functional_simulation<> sim {factory_32[cfg.simulator_name](proc_cfg)};
+    //     run_interactive_json_simulation(sim, cfg);
+    // } else {
+    //     functional_simulation<false, false, false, true> sim {factory_32[cfg.simulator_name](proc_cfg)};
+    //     sim.load_pfile(cfg.pfile);
+    //     sim.run();
+
+    //     if (cfg.print_reg_info)
+    //         print_reg_info(sim.get_reg_info());
+
+    //     if (cfg.print_exec_info)
+    //         print_exec_info(sim.get_exec_info());
+
+    //     if (cfg.run_checker) {
+    //         checker chk {sim, cfg.pfile, cfg.verbosity};
+    //         chk.run(cfg.checks);
+    //     }
+    // }
 }
